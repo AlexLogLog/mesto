ffb26e26260a1584bff8107c8110a88e7993a420
Alexandra Loginova <alexvoy.voy@yandex.ru>
2020-10-07 22:13:13 +0300

    3

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 let profile = document.querySelector('.profile__button-red');
 let popup = document.querySelector('.popup');
-let popup_close = document.querySelector('.popup__close');
+let popupClose = document.querySelector('.popup__close');
 
 let name = document.querySelector('.profile__name');
 let about = document.querySelector('.profile__about');
 
 let formElement = document.querySelector('.popup__form');
 
-let popup_name = document.querySelector('.popup__input_info_name'); 
-let popup_about = document.querySelector('.popup__input_info_about');
+let popupName = document.querySelector('.popup__input_info_name'); 
+let popupAbout = document.querySelector('.popup__input_info_about');
 
 function openPopup() {
-    popup_name.value  = name.textContent;
-    popup_about.value  = about.textContent;
+    popupName.value  = name.textContent;
+    popupAbout.value  = about.textContent;
     popup.classList.add('popup_opened');
 }
 
@@ -22,12 +22,12 @@ function closePopup() {
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    name.textContent = popup_name.value;
-    about.textContent = popup_about.value;
+    name.textContent = popupName.value;
+    about.textContent = popupAbout.value;
     closePopup();
 }
 
 profile.addEventListener('click', openPopup); 
-popup_close.addEventListener('click', closePopup); 
+popupClose.addEventListener('click', closePopup); 
 formElement.addEventListener('submit', formSubmitHandler);
 
